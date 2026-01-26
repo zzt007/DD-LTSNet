abeec79eab13c1e066aa9346839dfdc19c6c66c2
zzt007 <519174247@qq.com>
2026-01-26 18:17:06 +0800

    update the readme

--- train.py
+++ train.py
@@ -2,9 +2,9 @@ import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 if __name__ == '__main__':
-  model = YOLO('/home/2024-Zhong.zt/HHD/detection/DD-LTSNet/DD-LTSNet.yaml')
+  model = YOLO('./DD-LTSNet/DD-LTSNet.yaml')
   results = model.train(
-    data='/home/2024-Zhong.zt/HHD/detection/DD-LTSNet/cntsss.yaml',  #数据集配置文件的路径
+    data='./DD-LTSNet/cntsss.yaml',  #数据集配置文件的路径
     epochs=300, 
     imgsz=640, 
     workers=4,  
